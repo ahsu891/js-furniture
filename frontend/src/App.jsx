65b34dea85bd672c497538530165f117771a0688
ahsu891 <ahsu891@gmail.com>
2024-09-25 18:21:18 +0300

    setup the react router dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import Home from './pages/Home';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Navbar from './ui/Navbar';
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+]);
 export default function App() {
-  const he=10;
+  
   return (
     <div>
-      hello
+      <Navbar/>
+<RouterProvider router={router} />
     </div>
   )
 }
